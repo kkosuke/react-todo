c71452ddfd7a376f2cbeb7c6dac1ee7cd41818b3
kkosuke <kkosuke007@gmail.com>
2023-01-16 22:11:21 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,175 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Rule from "./components/Rule";
+import Todo from "./components/Todo";
+import TodoStatus from "./components/TodoStatus";
 
-function App() {
+const App = () =>{
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todoDetail, setTodoDetail] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState({});
+  function onTodoInputChange(event){
+    setTodoTitle(event.target.value)
+  }
+  function onTodoDetailTextareaChange(event){
+    setTodoDetail(event.target.value)
+  }
+  function generateTodoId() {
+    // 参考：https://qiita.com/t-yama-3/items/29bd686f2a8b3cb9e784
+    const date = new Date();
+    return date.getFullYear() + '' + date.getMonth() + 1 + '' + date.getDate() + '' + date.getHours() + '' + date.getMinutes() + '' + date.getSeconds()
+  }
+  function onTodoSubmit(event){
+    event.preventDefault();
+    if (todoTitle.trim()){
+      const newTodo = {
+        id: generateTodoId(),
+        status: 0,
+        title: todoTitle.trim(),
+        detail: todoDetail.trim(),
+      }
+      console.log(newTodo);
+      setTodos([...todos,newTodo]);
+      setTodoTitle("");
+      setTodoDetail("");
+    } else {
+      alert('TODOを入力してください');
+    }
+  }
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  // 押下したTODO以外を残す
+  function onClickDelete(id){
+    console.log(todos);
+    const removeItem = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(removeItem);
+  }
+  function onClickEdit(todo){
+    setIsEditing(true);
+    setCurrentTodo(todo);
+  }
+  function onEditTodoStatus(event) {
+    const thisTodo = {...currentTodo, ...{status:Number(event.target.value)}}
+    setCurrentTodo(thisTodo);
+  }
+  function onEditSubmit(event) {
+    event.preventDefault();
+    const updatedItem = todos.map((todo) => {
+      return todo.id === currentTodo.id ? currentTodo : todo;
+    });
+    setIsEditing(false);
+    setTodos(updatedItem);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="" style={{
+      margin: "20px"
+    }}>
+      <h1>TODOs</h1>
+      <section>
+        {isEditing ? (
+          <>
+          <h2>TODOを編集</h2>
+          <form onSubmit={onEditSubmit}>
+            <table>
+              <tbody>
+                <tr>
+                  <th>ステータス</th>
+                  <td>
+                    {Object.keys(TodoStatus).map(key => (
+                      <label key={key}>
+                        <input type="radio" name="TodoStatus" value={key}
+                        onChange={onEditTodoStatus}
+                        checked={String(key) === String(currentTodo.status)} />
+                        {TodoStatus[key]}
+                      </label>
+                    ))}
+                  </td>
+                </tr>
+                <tr>
+                  <th>タイトル</th>
+                  <td><input type="text" value={currentTodo.title} onChange={onTodoInputChange} /></td>
+                </tr>
+                <tr>
+                  <th>詳細</th>
+                  <td><textarea onChange={onTodoDetailTextareaChange} value={currentTodo.detail}></textarea></td>
+                </tr>
+                <tr>
+                  <th>&nbsp;</th>
+                  <td><button type="submit">更新</button></td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+          </>
+        ) : (
+          <>
+            <h2>TODOを作成</h2>
+            <form onSubmit={onTodoSubmit}>
+              <table>
+                <tbody>
+                  <tr>
+                    <th>タイトル</th>
+                    <td><input type="text" value={todoTitle} onChange={onTodoInputChange} /></td>
+                  </tr>
+                  <tr>
+                    <th>詳細</th>
+                    <td><textarea onChange={onTodoDetailTextareaChange} value={todoDetail}></textarea></td>
+                  </tr>
+                  <tr>
+                    <th>&nbsp;</th>
+                    <td><button type="submit">追加</button></td>
+                  </tr>
+                </tbody>
+              </table>
+            </form>
+          </>
+      )}
+
+      </section>
+      <section>
+        <h2>あなたのTODO</h2>
+        {todos.length ? (
+          <ul>
+            {todos.map((todo)=>(
+              <Todo key={todo.id}
+                todo={todo}
+                onClickEdit={onClickEdit}
+                onClickDelete={onClickDelete}
+              />
+            ))}
+          </ul>
+        ) : (
+          <p>TODOを入力してください。</p>
+        )}
+      </section>
+
+      <Rule />
+
+      <section>
+        <h2>やれたらいいな</h2>
+        <ul>
+          <li>todoの項目を用意に増やせるようになる</li>
+          <li>todoの編集のキャンセル</li>
+          <li>todoリストのその場で修正</li>
+          <li>並び替え</li>
+          <li>絞り込み検索（完全一致、含む、あいまい）</li>
+          <li>csvエクスポート ダウンロード、インポート</li>
+        </ul>
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Rule.js
@@ -0,0 +1,35 @@
+import styled from "styled-components"
+
+const SSection = styled.section`
+  background: #f6f6f6;
+  padding: 16px;
+  border-radius: 8px;
+`
+const SH2 = styled.h2`
+  margin:0;
+`
+
+export default function Rule() {
+  return (
+    <SSection>
+      <SH2>説明/学習法</SH2>
+      <p>TODOリストをつくってみる。ReactHooksを使う。</p>
+      <dl>
+        <dt>▼TODOリスト内の各TODOにほしい要素</dt>
+        <dd>・<strike>ID(連番もしくはuuidを設定)</strike>年月日日時で作成</dd>
+        <dd>・<strike>タイトル</strike></dd>
+        <dd>・<strike>ステータス(未着手、進行中、完了 など)</strike></dd>
+        <dd>・<strike>詳細</strike></dd>
+        <dt>▼ほしい機能</dt>
+        <dd>・<strike>TODOの追加</strike></dd>
+        <dd>・<strike>TODOの削除</strike></dd>
+        <dt>▼余裕があれば以下のようなカスタマイズをする</dt>
+        <dd>・<strike>TODOの編集機能</strike></dd>
+        <dd>・フィルター(ID、期限、ステータスで絞り込み) or ソート(ID、期限、ステータスで並べ替え)</dd>
+        <dd>・要素追加（内容、作成日、更新日など）</dd>
+        <dd>・<strike>ステータス変更でスタイル変更</strike></dd>
+        <dd>・<strike>TODOリストを1箇所(どのパーツでもOK)コンポーネント化してみる"</strike></dd>
+      </dl>
+    </SSection>
+  )
+}
